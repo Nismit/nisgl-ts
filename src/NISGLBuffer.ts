7e9fa1bbd4a8fd881053bf1be3dbb3e569fdc7cc
git <nismit.dev@gmail.com>
2019-05-16 23:28:45 -0700

    Update buffer class

--- src/NISGLBuffer.ts
+++ src/NISGLBuffer.ts
@@ -14,17 +14,10 @@ export class NISGLBuffer {
     return this._buffer;
   }
 
-  public createVertexBufferObject(source: ArrayBuffer | ArrayBufferView, usage: number = GL_CONST.STATIC_DRAW): void {
+  public createVertexBufferObject(type: number = GL_CONST.ARRAY_BUFFER, source: ArrayBuffer | ArrayBufferView, usage: number = GL_CONST.STATIC_DRAW): void {
     const gl = this._gl.getGLContext();
-    gl.bindBuffer(gl.ARRAY_BUFFER, this.getBuffer());
-    gl.bufferData(gl.ARRAY_BUFFER, source, usage);
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
-  }
-
-  public createIndexBufferObject(source: ArrayBuffer | ArrayBufferView, usage: number = GL_CONST.STATIC_DRAW) {
-    const gl = this._gl.getGLContext();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.getBuffer());
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, source, usage);
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+    gl.bindBuffer(type, this.getBuffer());
+    gl.bufferData(type, source, usage);
+    gl.bindBuffer(type, null);
   }
 }
