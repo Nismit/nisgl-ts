66fff0c3830a45256fd0221858720c75120bbecf
git <nismit.dev@gmail.com>
2019-05-20 17:35:58 -0700

    Update example code, wip

--- example/example.js
+++ example/example.js
@@ -1,13 +1,53 @@
 import { NISGL } from "../dist/nisgl-ts";
 
+const Vertex = `
+attribute vec3 position;
+
+void main() {
+	gl_Position = vec4(position, 1.0); 
+}`;
+
+const Fragment = `
+precision mediump float;
+const float PI = 3.1415926;
+uniform float time;
+uniform vec2  resolution;
+
+vec3 hsv(float h, float s, float v){
+	vec4 t = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+	vec3 p = abs(fract(vec3(h) + t.xyz) * 6.0 - vec3(t.w));
+	return v * mix(vec3(t.x), clamp(p - vec3(t.x), 0.0, 1.0), s);
+}
+
+void main() {
+  vec2 p = (gl_FragCoord.xy * 2.0 - resolution) / resolution;
+  vec3 line = vec3(0.0);
+  for(float fi = 0.0; fi < 50.0; ++fi){
+      float offset = fi * PI / 100.0;
+      float value = 1.0 + sin(time * fi * 0.15 + 0.1) * 0.5;
+      float timer = time * fi * 0.01;
+      vec3  color = hsv((fi + time) * 0.0175, 1.0, value);
+      line += 0.0025 / abs(p.y + sin(p.x * 1.0 + timer + offset) * 0.75) * color;
+  }
+
+  gl_FragColor = vec4(line, 1.0);
+}`;
+
+
 function init() {
-  console.log('test');
   const canvas = document.querySelector('#canvas');
   canvas.width = document.documentElement.clientWidth;
   canvas.height = window.innerHeight;
   const gl = canvas.getContext('webgl');
   const nisgl = new NISGL(gl);
-  console.log(nisgl);
+
+  const vertex = nisgl.createShader(gl.VERTEX_SHADER);
+  vertex.compile(Vertex);
+  const fragment = nisgl.createShader(gl.FRAGMENT_SHADER);
+  fragment.compile(Fragment);
+  const program = nisgl.createProgram();
+  program.linkProgram([vertex, fragment]);
+  console.log(program);
 }
 
 window.onload = (() => { init() });
