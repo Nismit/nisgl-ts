09d3e7b82c9616bbcb3ba3c97291f8625482d2e7
git <nismit.dev@gmail.com>
2019-11-24 01:10:51 -0800

    Add Vertex Buffer Object functions

--- src/NISGLProgram.ts
+++ src/NISGLProgram.ts
@@ -1,3 +1,4 @@
+import GL_CONST from './constants';
 import { NISGL } from './NISGL';
 import { NISGLShader } from './NISGLShader';
 
@@ -11,12 +12,12 @@ export class NISGLProgram {
     this._program = program;
   }
 
-  public deleteProgram(): void {
-    this._gl.getGLContext().deleteProgram(this._program);
+  public get getProgram(): WebGLProgram {
+    return this._program;
   }
 
-  public getProgram(): WebGLProgram {
-    return this._program;
+  public deleteProgram(): void {
+    this._gl.getGLContext().deleteProgram(this._program);
   }
 
   public useProgram(): void {
@@ -42,9 +43,44 @@ export class NISGLProgram {
     }
   }
 
+  public getAttributeLocation(name: string): GLint | void {
+    const gl = this._gl.getGLContext();
+    const program = this._program;
+
+    if (gl.getAttribLocation(program, name) !== -1) {
+      return gl.getAttribLocation(program, name);
+    } else {
+      this._gl.emitMessage('Not found Attribute Location');
+      return;
+    }
+  }
+
   public getUniformLocation(name: string) {
     const gl = this._gl.getGLContext();
 
     return gl.getUniformLocation(this._program, name);
+  }
+
+  public enableVertexAttribute(index: GLint) {
+    const gl = this._gl.getGLContext();
+    gl.enableVertexAttribArray(index);
+  }
+
+  public vertexAttributePointer(
+    index: GLint,
+    size: GLint,
+    type: number = GL_CONST.FLOAT,
+    normalized: boolean = false,
+    stride: number = 0,
+    offset: number = 0
+  ) {
+    const gl = this._gl.getGLContext();
+
+    gl.vertexAttribPointer(index, size, type, normalized, stride, offset);
+    // if (this._isEnabled) {
+
+    // } else {
+    //   this._gl.emitMessage('Not enabled vertex attribute yet');
+    // }
   }
 }
