bd57defba74bf0ea61962e629a34d7fef29f22f0
git <nismit.dev@gmail.com>
2020-01-19 00:42:11 -0800

    Add bindBuffer function into Buffer class

--- src/NISGLBuffer.ts
+++ src/NISGLBuffer.ts
@@ -4,6 +4,7 @@ import { NISGL } from './NISGL';
 export class NISGLBuffer {
   private _gl: NISGL;
   private _buffer: WebGLBuffer;
+  private _isCreatedBuffer: boolean = false;
 
   constructor(gl: NISGL, buffer: WebGLBuffer) {
     this._gl = gl;
@@ -24,6 +25,33 @@ export class NISGLBuffer {
   public deleteBuffer(): void {
     const gl = this._gl.context;
     gl.deleteBuffer(this._buffer);
+    this._isCreatedBuffer = false;
+  }
+
+  /**
+   * Bind buffer data
+   * Must be created buffer data before run this function
+   * @param type Bind type allow 'position' or 'index'
+   */
+  public bindBuffer(type: string): void {
+    const gl = this._gl.context;
+
+    if(this._isCreatedBuffer) {
+      switch(type) {
+        case 'position':
+          gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer);
+          break;
+        case 'index':
+          gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this._buffer);
+          break;
+        default:
+          this._gl.emitMessage('Incorrect bind type. Should be "position" or "index"');
+          return;
+      }
+    } else {
+      this._gl.emitMessage('This buffer is not created yet');
+      return;
+    }
   }
 
   /**
@@ -39,6 +67,7 @@ export class NISGLBuffer {
     gl.bindBuffer(gl.ARRAY_BUFFER, this._buffer);
     gl.bufferData(gl.ARRAY_BUFFER, source, usage);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
+    this._isCreatedBuffer = true;
   }
 
   public createVertexIndex(
@@ -49,5 +78,6 @@ export class NISGLBuffer {
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this._buffer);
     gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, source, usage);
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+    this._isCreatedBuffer = true;
   }
 }
