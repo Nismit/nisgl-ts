10e14fac717a4e18e50f6d2331c648de68c0927b
Nismit <nismit.dev@gmail.com>
2022-01-08 23:14:13 -0800

    Add prettier and formatted

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,8 @@
 module.exports = {
-  "transform": {
-    "\\.ts?$": "ts-jest"
+  transform: {
+    "\\.ts?$": "ts-jest",
   },
-  "testRegex": "[^.]*\.test\.ts?$",
-  "moduleFileExtensions": [
-    "ts",
-    "js",
-    "jsx",
-    "json",
-    "node"
-  ],
-  "collectCoverageFrom": [
-    "**/*.ts",
-    "!**/tests/**"
-  ]
-}
+  testRegex: "[^.]*.test.ts?$",
+  moduleFileExtensions: ["ts", "js", "jsx", "json", "node"],
+  collectCoverageFrom: ["**/*.ts", "!**/tests/**"],
+};
