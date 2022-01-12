e65d49512685418722a79718663f9ca04782fd84
Nismit <nismit.dev@gmail.com>
2022-01-11 22:11:28 -0800

    Rebuild

--- types/NISGLBuffer.d.ts
+++ types/NISGLBuffer.d.ts
@@ -1,29 +1,62 @@
-import { NISGL } from './NISGL';
+import { NISGLProgram } from "./NISGLProgram";
 export declare class NISGLBuffer {
     private _gl;
     private _buffer;
-    private _isCreatedBuffer;
-    constructor(gl: NISGL, buffer: WebGLBuffer);
+    private _bufferType;
+    private _usage;
+    private _attribs;
+    private _type;
+    constructor(gl: WebGLRenderingContext, buffer: WebGLBuffer, mode: "array" | "index", data?: BufferSource, type?: GLenum);
     /**
      * Get buffer instance
-     * @return Return WebGLBuffer
+     * @return {WebGLBuffer} WebGLBuffer
      */
     get getBuffer(): WebGLBuffer;
     /**
-     * Delete buffer
+     * Bind buffer
      */
-    deleteBuffer(): void;
+    bind(): void;
     /**
-     * Bind buffer data
-     * Must be created buffer data before run this function
-     * @param type Bind type allow 'position' or 'index'
+     * Set IndexBuffer Type
+     * @param {GLenum} type
      */
-    bindBuffer(type: string): void;
+    setType(type: GLenum): void;
     /**
-     * Attach vertex model data into the buffer
-     * @param source A model data
-     * @param usage Type of the model data, default GL_CONST.STATIC_DRAW
+     * Dispose the buffer
      */
-    createVertexPosition(source: ArrayBuffer | ArrayBufferView, usage?: number): void;
-    createVertexIndex(source: ArrayBuffer | ArrayBufferView, usage?: number): void;
+    dispose(): void;
+    /**
+     * Set data into the buffer
+     * @param source
+     */
+    data(source: BufferSource): void;
+    /**
+     * Store attribute data
+     * @param name Attibute name
+     * @param size Size of geometry object, Must be 1,2,3 or 4 (e.g. x,y,z object is 3)
+     * @param type Specifying the data type, default GL_CONST.FLOAT
+     * @param normalized Normalized VBO, default false
+     * @param stride default 0
+     * @param offset default 0
+     */
+    attrib(name: string, size: number, type?: GLenum, normalized?: boolean, stride?: number, offset?: number): void;
+    /**
+     * Subscribe VBO attributes
+     * @param {NISGLProgram} program
+     */
+    attribPointer(program: NISGLProgram): void;
+    /**
+     * Shortcut of Draw function
+     * @param {GLenum} mode
+     * @param {number} count
+     * @param {number} offset
+     */
+    draw(mode: GLenum, count: number, offset?: number): void;
+    drawPoints(count: number, offset: number): void;
+    drawLines(count: number, offset: number): void;
+    drawLineLoop(count: number, offset: number): void;
+    drawLineStrip(count: number, offset: number): void;
+    drawTriangles(count: number, offset: number): void;
+    drawTriangleStrips(count: number, offset: number): void;
+    drawTriangleFan(count: number, offset: number): void;
 }
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,2 @@
-import GL_CONST from './constants';
-export { GL_CONST };
-export { NISGL } from './NISGL';
-export { NISGLShader } from './NISGLShader';
-export { NISGLProgram } from './NISGLProgram';
-export { NISGLBuffer } from './NISGLBuffer';
+import { NISGL } from "./NISGL";
+export default NISGL;
