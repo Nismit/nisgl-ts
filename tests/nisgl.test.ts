5db562d26aa046a6aaa0621d76f63e6e45587d4d
Nismit <nismit.dev@gmail.com>
2022-01-20 21:28:07 -0800

    Rewrite test

--- tests/nisgl.test.ts
+++ tests/nisgl.test.ts
@@ -1,52 +1,50 @@
-/**
- * gl library has affected Node internal library bug
- * Node version should be under v12.13.0 to avoid the bug for now.
- * Ref: https://github.com/stackgl/headless-gl/issues/180 
- */
-
 import WebGLContext from "gl";
 import { NISGL } from "../src/NISGL";
-import { NISGLShader } from "../src/NISGLShader";
 import { NISGLProgram } from "../src/NISGLProgram";
+import { NISGLBuffer } from "../src/NISGLBuffer";
 
-describe('NISGL Functions Test', () => {
+describe("NISGL Functions Test", () => {
   let gl: WebGLRenderingContext;
   let nisgl: NISGL;
 
   beforeAll(() => {
     gl = WebGLContext(500, 250);
     nisgl = new NISGL(gl);
-});
+  });
 
-  it('Initalize Test', () => {
+  it("Initalize Test", () => {
     expect(nisgl).toBeInstanceOf(NISGL);
   });
 
-  it('Should get WebGL Rendering Context', () => {
+  it("Should get WebGL Rendering Context", () => {
     const context = nisgl.context;
     expect(context).toBe(gl);
   });
 
-  it('Context clear test', () => {
+  it("Context clear test", () => {
     nisgl.clear(1.0, 0.5, 0.5, 1.0, 0.5);
-    const colorArray = nisgl.context.getParameter(nisgl.context.COLOR_CLEAR_VALUE);
-    const convertedArray =  Array.from(colorArray);
-    expect([1.0, 0.5, 0.5, 1.0]).toEqual(expect.arrayContaining(convertedArray));
+    const colorArray = nisgl.context.getParameter(
+      nisgl.context.COLOR_CLEAR_VALUE
+    );
+    const convertedArray = Array.from(colorArray);
+    expect([1.0, 0.5, 0.5, 1.0]).toEqual(
+      expect.arrayContaining(convertedArray)
+    );
   });
 
-  it('Unexpected Error with createShader', () => {
+  it("Unexpected Error with createProgram", () => {
     expect(() => {
-      nisgl.createShader(32);
+      nisgl.createProgram("32");
     }).toThrowError();
   });
 
-  it('Create shader instance', () => {
-    const shader = nisgl.createShader(nisgl.context.FRAGMENT_SHADER);
-    expect(shader).toBeInstanceOf(NISGLShader);
-  });
-
-  it('Create program instance', () => {
+  it("Create program instance", () => {
     const program = nisgl.createProgram();
     expect(program).toBeInstanceOf(NISGLProgram);
   });
-})
+
+  it("Create buffer instance", () => {
+    const buffer = nisgl.arrayBuffer(new Float32Array([1.0]));
+    expect(buffer).toBeInstanceOf(NISGLBuffer);
+  });
+});
