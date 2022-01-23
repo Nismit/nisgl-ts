c1e42f9a046e86261456ffee9d56ef0133fded54
Nismit <nismit.dev@gmail.com>
2022-01-23 13:38:58 -0800

    Rebuild types

--- types/NISGLFrameBuffer.d.ts
+++ types/NISGLFrameBuffer.d.ts
@@ -7,13 +7,42 @@ export declare class NISGLFrameBuffer {
     private _height;
     private _attachments;
     constructor(gl: WebGLRenderingContext, width?: number, height?: number);
+    /**
+     * Bind FrameBuffer
+     */
     bind(): void;
+    /**
+     * Unbind FrameBuffer
+     */
     unbind(): void;
+    /**
+     * Dispose the FrameBuffer instance
+     */
     dispose(): void;
+    /**
+     * Resize
+     * If it has attachment(s), the attachments runs resize as well
+     * @param {number} width
+     * @param {number} height
+     */
     resize(width: number, height: number): void;
+    /**
+     * Set Depth Buffer
+     */
     attachDepth(): void;
+    /**
+     * Set Texture Buffer
+     */
     attachTexture(): void;
+    /**
+     * Get Depth Buffer
+     * @returns {WebGLRenderbuffer | null}
+     */
     getDepth(): AttachmentType | null;
+    /**
+     * Get Texture Buffer
+     * @returns {NISGLTexture | null}
+     */
     getTexture(): AttachmentType | null;
 }
 declare class NISGLRenderBuffer {
@@ -22,9 +51,23 @@ declare class NISGLRenderBuffer {
     private _width;
     private _height;
     constructor(gl: WebGLRenderingContext);
+    /**
+     * Bind RenderBuffer
+     */
     bind(): void;
+    /**
+     * Dispose RenderBuffer
+     */
     dispose(): void;
+    /**
+     * Resize RenderBuffer
+     * @param {number} width
+     * @param {number} height
+     */
     resize(width: number, height: number): void;
+    /**
+     * Set width/height into RenderBuffer
+     */
     storage(): void;
 }
 export {};
